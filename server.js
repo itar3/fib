3d55fc07defdcbf00babbce874aa86d35f73c60a
itar3 <itar3@student.monash.edu>
2020-12-19 11:46:10 +0600

    update

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
-app.listen(8080, () =>{
-    console.log('listening to 8080')
-});
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const fib = require('./build/Release/fib');
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/page.html');
+});
+
+app.post('/', (req,res) =>{
+    const num = req.body.fib;
+    
+    const toReturn = fib.fibbonaci(Number(num));
+    res.send(toReturn.toString());
+
+});
+
+app.listen(8080, () => {
+    console.log('listening to 8080')
 });
